c7e5212440d5794715f104cf7f5ea728fb90c4ca
OT4EHAW <tokarev.danila.maximovich@gmail.com>
2022-04-04 13:32:14 +0300

    feat: изменить контекст

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,10 @@
       <v-card>
         <v-card-title class="headline ">
           <SafeLogo class="d-flex justify-center mb-10"/>
-          Криптографический алгоритм «Кузнечик»
+          Ваши пароли защищены шифрованием.
         </v-card-title>
         <v-card-text>
-          <p>
-            Российский стандарт шифра с длинной блока 128 бит - <a
-              href="https://protect.gost.ru/v.aspx?control=8&baseC=-1&page=0&month=-1&year=-1&search=&RegNum=1&DocOnPageCount=15&id=224244&pageK=F56A2B0D-ADE5-46EF-A808-6408E9E0B045"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            ГОСТ 34.12-2018
-            </a>
 
-          </p>
-
-          <p>Ваши пароли под надёжной защитой.</p>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
